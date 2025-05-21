11ecd1699e85872fbb38044b04d2544eae7ef678
acousticus <mitchell.t.foster@gmail.com>
2025-05-21 16:15:59 +0100

    update rota 2025-05-21

--- run.R
+++ run.R
@@ -2,7 +2,7 @@
 
 x %>% color_rota(specific_start_date = today()-7,
                  publish_until = dmy('15022026'),
-                 weeks_ahead = 30) %>%
+                 weeks_ahead = 52) %>%
   scroll_box(width = "100%", height = "500px") %>%
   save_kable(file = "../dcnrota/published_rota.html", self_contained = T)
 
